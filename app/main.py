db4276ae9a7a30f420d3f50965b2e23697a00618
viktor-77 <vumanskiy0664041341@gmail.com>
2024-07-29 14:33:17 +0300

    Resolved

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import json
 from decimal import Decimal
 
 
-def calculate_profit(filename:str) -> None:
+def calculate_profit(filename: str) -> None:
 
     with open(filename, "r") as f:
         trades_data = json.load(f)
